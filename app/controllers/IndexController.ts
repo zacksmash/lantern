73ed0d693045840ce90e9d784a0452106cdcf003
zacksmash <zachary.warren@me.com>
2025-11-14 15:13:21 -0700

    WIP 🚧

--- app/controllers/IndexController.ts
+++ app/controllers/IndexController.ts
@@ -1,6 +1,6 @@
 export class IndexController {
 	invoke(): Response {
-		return new Response(`<div>${config("app.env")}</div>`, {
+		return new Response(`<div>Hello, world!</div>`, {
 			headers: { "Content-Type": "text/html" },
 			status: 200,
 		});
