8e0c64e4959820785f249ffb0abf1bf1841a6103
zacksmash <zachary.warren@me.com>
2025-11-14 17:51:38 -0700

    WIP 🚧

--- @core/Routing/Facades/Route.ts
+++ @core/Routing/Facades/Route.ts
@@ -1,4 +1,5 @@
 import { ContainerTokens } from "@core/Application/ContainerTokens";
+import type { Router } from "@core/Routing/Router";
 import { app } from "@root/bootstrap/app";
 
-export const Route = app.resolve(ContainerTokens.Router);
+export const Route = app.resolve(ContainerTokens.Router) as Router;
